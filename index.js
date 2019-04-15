58e1b69b5a89c82d8aae398bb41b12289ea6edf8
Thiago Krüger <thiagork@msn.com>
2019-04-15 22:19:46 +0200

    Changed DB address to .env file, updated /documentation

--- index.js
+++ index.js
@@ -228,7 +228,7 @@ app.get('/documentation', (req, res) => {
 });
 
 {
-    const port = process.env.APP_PORT || 3000;
+    const port = process.env.PORT || 3000;
     app.listen(port, '0.0.0.0', () => {
         console.log(`Your app is listening on port ${port}.`);
     });
