116d714892837fe628b49f36868bea0275269c81
Thiago Krüger <thiagork@msn.com>
2019-04-19 22:32:26 +0200

    removed authentication requirement for /movie

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import axios from 'axios';
 
-class MainView extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // Initialize the state to an empty object to destructure it later
-        this.state = {};
+export class MainView extends React.Component {
+    componentDidMount() {
+        axios.get('https://myflix-mern.herokuapp.com/movies')
+        .then(response => {
+            this.setState({
+                movies: response.data
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
     }
 
     render() {
+        const { movies } = this.state;
+
+        if (!movies) return <div className="main-view"/>;
+
         return (
-            <div className='main-view'></div>
+            <div className="main-view">
+                { movies.map(movie=> (
+                    <div className="movie-card" key={movie._id}>{movie.Title}</div>
+                ))}
+            </div>
         );
     }
 }
