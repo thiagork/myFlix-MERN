47d08f30dfb0f7acd7fc19a4e3e1dfc5cf81977a
Thiago Krüger <thiagork@msn.com>
2019-08-19 21:46:15 +0200

    Updates

---
+++ precache-manifest.62e064e32cd852f70cc71048ebd3e8a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f75540ce538964216e88a6560fd369",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72800e2fdad6f478f8e2",
+    "url": "./static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7db5adf8b1220ce7f792",
+    "url": "./static/css/main.b8a91b22.chunk.css"
+  },
+  {
+    "revision": "72800e2fdad6f478f8e2",
+    "url": "./static/js/2.d42a9737.chunk.js"
+  },
+  {
+    "revision": "7db5adf8b1220ce7f792",
+    "url": "./static/js/main.b464b6f1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
